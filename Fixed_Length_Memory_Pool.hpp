f48c7ee2fa98d81c7e022a0fda610346de5f2344
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2023-06-02 02:56:02 +0800

    修改了部分内容

--- Fixed_Length_Memory_Pool.hpp
+++ Fixed_Length_Memory_Pool.hpp
@@ -1,54 +1,5 @@
 #pragma once
 /*
-用位图索引已分配内存块链表节点，用空闲内存栈节点索引内存位图
-
-
-数据成员：
-void* mem_pool：一个内存池，按照固定大小来分块
-void** mem_bitmap：一个内存位图，存储已分配内存块的链表节点地址，如果地址是NULL，则代表内存块空闲
-void** free_mem_stack：一个栈数组，存储待分配内存块指针
-struct listnode{void*,listnode*} alloc_mem_list：一个链表，存储已分配内存块
-
-size_t stack_top：一个数组下标，用来存储栈数组栈顶
-size_t fixed_length：用户初始化时确定的长度
-
-
-接口成员：
-初始化：从用户传入的长度初始化内存池和长度，从系统分配并初始化全0的内存位图，从系统分配并初始化栈数组，初始化栈数组栈顶下标，从系统分配并初始化链表
-析构：直接释放内存池，设置所有成员为0
-
-
-
-分配：从栈数组顶部弹出一个待分配内存块，插入已分配内存块链表，并设置内存位图对应的内存块分配状态与插入的已分配内存块链表的节点地址
-释放：从内存位图中寻找此释放地址对应内存块的分配状态，如果为已使用，则利用该位图节点中存储的已分配链表节点地址索引到链表节点执行删除操作，并压入待分配内存块栈数组顶部
-
-释放全部：删除已分配内存块链表是所有节点，直接设置栈数组
-
-
-实现细节：
-当前暂时不考虑内存池用完的情况，等项目雏形做完后再处理此问题（预计需要用链表衔接所有的内存池）
-使用双链表，可以让链表的两头分别存储不同的数据（正向是已分配内存块，反向是空闲内存块）优化掉栈数组，并增加一个元素存储当前内存池中已经分配出去的最高地址的后继内存块地址，用于懒惰初始化空闲内存块链表部分和勤快分配计算
-
-初始化栈数组：
-			方案1：全零初始化，然后初始化栈顶元素为内存池起始地址，分配内存时，先检查栈顶后的一个元素是否为0，如果是则利用当前元素的值计算后继元素的值然后弹出当前元素（懒惰初始化，勤快分配计算）
-			方案2：完全初始化，利用循环给数组每个元素都初始化空闲内存地址，后续分配时直接使用（勤快初始化，无分配计算耗时）
-初始化链表：
-			方案1：使用游标数组模拟的静态链表，给整个链表待分配空闲节点链接做初始化
-			方案2：使用真链表，并在使用时从系统分配内存，并在后续释放节点时插入已释放节点链表而不是真的释放（懒惰释放，减少重分配耗时）
-链表实现：
-			方案1：使用单链表，内存位图存储单链表的前驱节点而不是当前节点以便删除节点时能够修改前驱节点，但是这种情况下如果其它地方删除了当前位图存储的前驱节点，则会导致错误
-			方案2：使用双链表，内存位图直接存储当前节点，删除时直接修改节点
-
-
-耗时：
-分配：O(1)
-释放：O(1)
-
-
-*/
-
-/*
-经过思考，已分配内存块链表并无意义，进行重新设计
 用位图索引检查待释放内存地址和状态是否正确，用空闲内存栈节点索引内存位图
 也就是内存位图用于检查释放地址是否合法，栈用于存储待分配内存块
 
@@ -87,8 +38,6 @@ size_t fixed_length：用户初始化时确定的长度
 #include <new>
 #include <string.h>
 
-
-#define SENTINEL_POINTER ((void *)(NULL + 1))//哨兵指针
 
 struct default_alloc
 {
@@ -162,6 +111,7 @@ public:
 	using RetPoint_Type = Type;
 	static constexpr size_t szLazyInitExtraRequireSize = bLazyInit * sizeof(*pArrMemBlockStack);//懒惰初始化所需的额外内存
 	static constexpr size_t szManageMemBlockRequireSize = sizeof(*bArrMemBlockBitmap) + sizeof(*pArrMemBlockStack);//管理一个内存块所需的管理内存大小
+	static constexpr size_t szAlignmentSize = szAlignment;//对齐内存的边界
 
 	FixLen_MemPool(size_t _szMemBlockFixSize = sizeof(Type), size_t _szMemBlockPreAllocNum = 1024) :
 		szMemBlockFixSize(_szMemBlockFixSize),
@@ -305,11 +255,13 @@ public:
 		//设置栈数据
 		if constexpr (bLazyInit == true)
 		{
+#define SENTINEL_POINTER ((void *)(NULL + 1))//哨兵指针
 			//懒惰初始化
 			pArrMemBlockStack[szStackTop] = pMemPool;//设置起始地址，剩下的全0
 			memset(&pArrMemBlockStack[szStackTop + 1], NULL, szMemBlockNum * sizeof(*pArrMemBlockStack));//把中间都设置成NULL
 			pArrMemBlockStack[szMemBlockNum] = SENTINEL_POINTER;//设置尾部为一个非NULL的非法地址（实际上并不会被分配出去，仅用于做哨兵标记）
 			//注意：这种情况下pArrMemBlockStack[szMemBlockNum]并不会超尾访问，因为初始化分配内存时会加上bLazyInit
+#undef SENTINEL_POINTER
 		}
 		else
 		{
@@ -354,5 +306,3 @@ public:
 		return szStackTop;
 	}
 };
-
-#undef SENTINEL_POINTER
