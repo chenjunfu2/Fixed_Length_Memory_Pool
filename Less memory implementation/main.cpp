0bb180ecf861f29fcc3e1b6871dbdba261126537
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2023-08-17 02:10:37 +0800

    更少内存消耗的内存池实现

---
+++ Less memory implementation/main.cpp
@@ -0,0 +1,15 @@
+
+int Example_Fixed_Length_Memory_Pool(void);
+int Example_Automatic_Expand_Fixed_Length_Memory_Pool(void);
+
+int test_0(void);
+int test_1(void);
+int test_2(void);
+int test_3(void);
+int test_4(void);
+
+
+int main(void)
+{
+	return test_3();
+}
